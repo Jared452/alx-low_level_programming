ab5ef705a6abf54dd451da24d6f7aa90e17da30f
Jared452 <kpnyon@gmail.com>
2022-12-10 13:00:54 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print alphabet
+ * Return: Always 0
+ */
+int main(void)
+{
+	for (a=0; a<=10; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
