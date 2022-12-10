2bb6618f7d627b27ea7adb362db341a48e7909b5
Jared452 <kpnyon@gmail.com>
2022-12-10 13:25:59 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print alphabet
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
