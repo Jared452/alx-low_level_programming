50f3ea5b3473211fb5fa2f392c4138aa73aaa4d7
Jared452 <kpnyon@gmail.com>
2022-12-13 07:07:00 -0800

    v

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _putchar - prints _putchar and a new line
+ * @c: the character to print
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
