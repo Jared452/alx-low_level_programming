ee75530b08db15a8a888637158e29c938bb036cb
Jared452 <kpnyon@gmail.com>
2022-12-15 03:06:20 -0800

    .5-more_numbers.c.swp

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,14 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, ro;
 
-	for (j = 0, j <= 9;j++)
+	for (ro = 0, ro <= 9; ro++)
 	{
 		for (i = 0, i <= 14; i++)
 		{
 			if (i > 9)
-				_putchar(i / 10 + '0');
+			_putchar(i / 10 + '0');
 			_putchar(i % 10 + '0');
 		}
 	}
