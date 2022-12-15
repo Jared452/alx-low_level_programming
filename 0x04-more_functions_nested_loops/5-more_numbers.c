746cb541d3a7c6f29bd9c31518971e12d0bd5504
Jared452 <kpnyon@gmail.com>
2022-12-15 03:16:01 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,16 +7,19 @@
  */
 void more_numbers(void)
 {
-	int i, ro;
 
-	for (ro = 0; ro <= 9; ro++)
-	{
-		for (i = 0; i <= 14; i++)
-		{
-			if (i > 9)
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
-		}
-	}
-	_putchar('\n');
+int i, ro;
+
+for (ro = 0; ro <= 9; ro++)
+{
+for (i = 0; i <= 14; i++)
+{
+if (i > 9)
+
+_putchar(i / 10 + '0');
+
+_putchar(i % 10 + '0');
+}
+_putchar('\n');
+}
 }
