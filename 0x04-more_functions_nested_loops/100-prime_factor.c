02065bf7a938f895ca68662c475ce4c0e677aee4
Jared452 <kpnyon@gmail.com>
2022-12-15 09:36:44 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,25 +1,52 @@
 #include <stdio.h>
+
+
 /**
- * main - print the largest prime number
+ * isprime - check if integer is divisible by any number other than 1.
+ * @n: integer to check
  *
- * Return: Always
+ * Return: 1 if n is prime,
+ * otherwise 0.
+ */
+int isprime(long n)
+{
+	long i;
+	long i_max = n / 2;
+
+	for (i = 2; i < i_max; ++i)
+	{
+		if (n % i == 0)
+			return (0);
+	}
+	return (1);
+}
+
+
+/**
+ * largest_prime_factor - find the largest prime_factor of an integer.
+ * @n: integer to factor
+ *
+ * Return: largest prime factor of n.
+ */
+long largest_prime_factor(long n)
+{
+	long i = 2;
+
+	while (!isprime(n))
+	{
+		while (n % i || !isprime(i))
+			++i;
+		n /= i;
+	}
+	return (n);
+}
+/**
+ * main - find and print the largest prime factor of 612852475143
+ *
+ * Return: Always 0.
  */
 int main(void)
-long int prime, div;
-prime = 612852475143;
-while (div < (prime / 2))
 {
-if ((prime % 2) == 0)
-{
-prime /= 2;
-continue;
-}
-for (div = 3; div < (prime / 2); div += 2
-{
-if ((prime % div) == 0
-prime /= div;
-}
-}
-printf("%d\n", prime);
-return (0);
+	printf("%ld\n", largest_prime_factor(612852475143));
+	return (0);
 }
