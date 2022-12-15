65b5e4ca2ace14aa8ba7c40e051607c56d36bcc2
Jared452 <kpnyon@gmail.com>
2022-12-15 03:36:49 -0800

    for (ro = 0; ro <= 9; ro++)

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that draws diagonal line
+ * @n: input number
+ * Return: diagonal
+ */
+void print_diagonal(int n)
+{
+
+int co, cp;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+
+else
+{
+for (co = 1; co <= n; co++)
+{
+for (cp = 1; cp <= n; cp++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+}
