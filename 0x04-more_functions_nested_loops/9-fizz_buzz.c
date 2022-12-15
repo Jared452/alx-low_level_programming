305766b14306da3ff8f2c636c6c062bfd1f37bbb
Jared452 <kpnyon@gmail.com>
2022-12-15 04:59:11 -0800

    printf

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,28 +9,24 @@
  */
 int main(void)
 {
-	int n;
+int i;
+char f[] = "Fizz";
+char b[] = "Buzz";
+char fb[] = "FizzBuzz";
 
-	n = 1;
-	printf("%d", n);
-	for (n = 2; n <= 100; n++)
-	{
-		if ((n % 3 == 0) && (n % 5 == 0))
-		{
-			printf(" FizzBuzz");
-		}
-		else if (n % 3 == 0)
-		{
-			printf(" Fizz");
-		}
-		else if (n % 3 == 0)
-		{
-			printf(" Buzz");
-		}
-		else
-		{
-			printf(" %d, n");
-	}
-	printf("\n");
-	return (0);
+for (i = 1; i <= 100; i++)
+{
+if (i == 100)
+printf("%s", b);
+else if ((i % 3 == 0) && (i % 5 == 0))
+printf("%s ", fb);
+else if (i % 3 == 0)
+printf("%s ", f);
+else if (i % 5 == 0)
+printf("%s ", b);
+else
+printf("%d ", i);
+}
+printf("\n");
+return (0);
 }
